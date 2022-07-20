9d45d895e69136a8f475324036a62c3929c94b4b
samdevbeltran <samigamedev96@gmail.com>
2022-07-20 14:41:19 -0500

    rentals page process 2

--- js/scooter-inf.js
+++ js/scooter-inf.js
@@ -1,17 +1,15 @@
-function getVehicleHtml(){
-    return '<div class="scoot-grid">'+
-        '<div class="vehicle-container">'+
+function getVehicleHtml(obj){
+    return '<div class="vehicle-container">'+
             '<div class="veh-img-cont">'+
                 '<picture>'+
-                    '<source media="(max-width:767px)" srcset="images/honda-metro-350.jpg">'+
-                    '<img src="images/honda-metro-350.jpg" alt="logo">'+
+                    '<img src="images/'+obj["src"]+'" alt="logo">'+
                 '</picture>'+
             '</div>'+
             '<div class="veh-info-cont">'+
                 '<table>'+
                     '<tbody>'+
                         '<tr>'+
-                            '<td class="veh-name"></td>'+
+                            '<td class="veh-name">'+obj["model"]+'</td>'+
                             '<td class="yellow-backG" style="text-align: center;" colspan="2"><strong>Reservation</strong></td>'+
                             '<td class="blue-backG" style="text-align: center;" colspan="2"><strong>Walk-In</strong></td>'+
                         '</tr>'+
@@ -23,23 +21,34 @@ function getVehicleHtml(){
                             '<td class="blue-backG" style="text-align: center;"><strong>Full Day</strong></td>'+
                         '</tr>'+
                         '<tr>'+
-                            '<td class="veh-people" style="text-align: center;">1</td>'+
-                            '<td class="veh-re-half" style="text-align: center;">$20</td>'+
-                            '<td class="veh-re-full" style="text-align: center;">$30</td>'+
-                            '<td class="veh-wa-half" style="text-align: center;">$25</td>'+
-                            '<td class="veh-wa-full" style="text-align: center;">$35</td>'+
+                            '<td class="veh-people" style="text-align: center;">'+obj["max_person"]+'</td>'+
+                            '<td class="veh-re-half" style="text-align: center;">'+ obj["reservation"]["half-day"]+'</td>'+
+                            '<td class="veh-re-full" style="text-align: center;">'+obj["reservation"]["full-day"]+'</td>'+
+                            '<td class="veh-wa-half" style="text-align: center;">'+obj["walk-in"]["half-day"]+'</td>'+
+                            '<td class="veh-wa-full" style="text-align: center;">'+obj["walk-in"]["full-day"]+'</td>'+
                         '</tr>'+
                     '</tbody>'+
                 '</table>'+
                 '<button>Reserve</button>'+
             '</div>'+
-        '</div>'+
-    '</div>';
+        '</div>';
 }
+const url = "https://samdevbeltran.github.io/samscoot.github.io/json/scooter-api.json"
 
-fetch('../json/scooter-api.json').then(response =>{
-    
-    console.log(response.json())
+fetch(url).then(response => response.json() ).then(obj =>{
+    let honda = document.querySelector("#honda-content");
+    let atv = document.querySelector("#ATV-content");
+    let jeep = document.querySelector("#jeep-content");
 
+    obj["honda-motor-scooters"].forEach(element => {
+        honda.innerHTML += getVehicleHtml(element);
+    });
+
+    obj["ATV"].forEach(element => {
+        atv.innerHTML += getVehicleHtml(element);
+    });
+
+    obj["Jeep rentals"].forEach(element => {
+        jeep.innerHTML += getVehicleHtml(element);
+    });
 })
-document.querySelector(".honda-section").innerHTML += getVehicleHtml();
